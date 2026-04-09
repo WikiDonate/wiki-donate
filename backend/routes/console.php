bca5cc581aee126e4fceb36d4ad442b23bf56a56
Shahaj <gshahaj@gmail.com>
2026-04-09 20:35:59 +0600

    Donation Formulas:: Organization scrap

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -1,11 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-// Artisan::command('inspire', function () {
-//     $this->comment(Inspiring::quote());
-// })->purpose('Display an inspiring quote')->hourly();
-
 Schedule::command('queue:work --max-time=3599')->hourly();
+Schedule::command('paypal:scrape-charities')->hourly();
