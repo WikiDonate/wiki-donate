4c67dfba97df61ca6f9dc31474134e52deb0588b
Shahaj <gshahaj@gmail.com>
2026-06-02 12:59:06 +0600

    Backend:: Schedule issue solved

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('queue:work --max-time=3599')->hourly();
-Schedule::command('paypal:scrape-charities')->dailyAt('1:00');
+Schedule::command('queue:work --max-time=3599 --tries=3 --timeout=120')->hourly();
+// Schedule::command('paypal:scrape-charities')->dailyAt('1:00');
